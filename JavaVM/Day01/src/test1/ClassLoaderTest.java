0108769f666b2f9c67ee4aa4e5ddd0fe5c5675c5
CYZ <1822550890@qq.com>
2021-02-10 21:37:36 +0000

    系统类加载器、扩展类加载器

--- JavaVM/Day01/src/test1/ClassLoaderTest.java
+++ JavaVM/Day01/src/test1/ClassLoaderTest.java
@@ -1,8 +1,10 @@
 package test1;
 
 import sun.misc.Launcher;
+import sun.security.ec.CurveDB;
 
 import java.net.URL;
+import java.security.Provider;
 
 /**
  * @author Chenyang
@@ -16,5 +18,17 @@ public class ClassLoaderTest {
         for (URL urL : urLs) {
             System.out.println(urL.toExternalForm());
         }
+        //从上面路径中任意选择一个类，查看其类加载器
+        ClassLoader classLoader = Provider.class.getClassLoader();
+        System.out.println(classLoader);
+
+        System.out.println("********扩展类加载器********");
+        String property = System.getProperty("java.ext.dirs");
+        for (String path : property.split(";")) {
+            System.out.println(path);
+        }
+        //从上面路径中任意选择一个类，查看其类加载器: 扩展类加载器
+        ClassLoader classLoader1 = CurveDB.class.getClassLoader();
+        System.out.println(classLoader1);//sun.misc.Launcher$ExtClassLoader@677327b6
     }
 }
