6421a7293b56110ed97d2835e4361ff2047a0f3f
CYZ <1822550890@qq.com>
2021-02-12 11:59:38 +0000

    程序计数器

---
+++ JavaVM/Day01/src/test3/PCRegisterTest.java
@@ -0,0 +1,19 @@
+package test3;
+
+/**
+ * 程序计数器
+ *
+ * @author Chenyang
+ * @create 2021-02-11-16:54
+ */
+public class PCRegisterTest {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+
+        String s = "abc";
+        System.out.println(i);
+        System.out.println(k);
+    }
+}
