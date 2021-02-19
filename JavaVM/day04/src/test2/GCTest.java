343500301a68436cbfeefa6a8f9090522adcbd23
CYZ <1822550890@qq.com>
2021-02-19 12:07:48 +0000

    通过指令设置GC

---
+++ JavaVM/day04/src/test2/GCTest.java
@@ -0,0 +1,14 @@
+package test2;
+
+import java.util.Scanner;
+
+/**
+ * @author Chenyang
+ * @create 2021-02-18-14:31
+ */
+public class GCTest {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        new Scanner(System.in).next();
+    }
+}
