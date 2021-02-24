f0b18098e8ebe9bba8aaaed609072e7a7d21b582
CYZ <1822550890@qq.com>
2021-02-24 10:02:52 +0000

    透过字节码看代码执行

---
+++ JavaVM/day05/src/test1/HexEditor.java
@@ -0,0 +1,15 @@
+package test1;
+
+/**
+ * @author Chenyang
+ * @create 2021-02-19-17:28
+ */
+public class HexEditor {
+
+    private int num = 1;
+
+    public int add() {
+        num = num + 2;
+        return num;
+    }
+}
---
+++ JavaVM/day05/src/test2/ExpressionTest.java
@@ -0,0 +1,17 @@
+package test2;
+
+/**
+ * @author Chenyang
+ * @create 2021-02-21-17:54
+ */
+public class ExpressionTest {
+
+    public void method1(){
+        int a = 1;
+        if(a==10){
+            a = 20;
+        }else{
+            a = 30;
+        }
+    }
+}
